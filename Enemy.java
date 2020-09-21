1828521eb438b600babc0e003274f2e92a2921a7
laura-estep <tiny.kim@gmail.com>
2020-09-21 12:56:00 -0400

    made enemy stub file

---
+++ Enemy.java
@@ -0,0 +1,14 @@
+/*
+ * Enemy.java
+ * CS 321 Team 9
+ * creates the Enemy class
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Enemy {
+    public static void main (String[] args){
+        
+    }
+}

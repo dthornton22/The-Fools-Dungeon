e3ba662f6cc6f551193113a32147b6a977fd8f8a
laura-estep <estep.e.laura@gmail.com>
2020-11-29 16:34:36 -0500

    window will open in center of screen

--- src/main/java/my/test/Frame.java
+++ src/main/java/my/test/Frame.java
@@ -8,6 +8,7 @@ package main.java.my.test;
 
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import java.awt.Toolkit;
 
 /**
  * Extends JFrame to create application window
@@ -26,6 +27,7 @@ public class Frame extends JFrame
 		super("The Fool's Dungeon");
 		FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
 		setLayout(flowLayout);
+                setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - 950) / 2, (Toolkit.getDefaultToolkit().getScreenSize().height - 680) / 2);    
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
